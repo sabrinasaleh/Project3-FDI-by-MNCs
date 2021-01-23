19750cb6ff25f2f29a10e7c027fd3e95ecf3f6bd
Rina Rin <68408283+sabrinasaleh@users.noreply.github.com>
2021-01-23 15:31:30 -0600

    added updates for prediction page

--- app.py
+++ app.py
@@ -10,12 +10,59 @@ model = joblib.load("Artifacts/model.joblib")
 #     model_input = [year, country_id, region_id, globalization_100]
 #     return model.predict([model_input])[0]
 
+globalization_dict = {"United Kingdom": 89,
+"Austria": 89,
+"Belgium": 90,
+"Denmark": 88,
+"Finland": 88,
+"France": 88,
+"Germany": 89,
+"Ireland": 86,
+"Italy": 83,
+"Luxembourg": 83,
+"Netherlands": 91,
+"Norway": 85,
+"Portugal": 85,
+"Russia": 72,
+"Spain": 84,
+"Sweden": 89,
+"Switzerland": 91,
+"Greece": 83,
+"Hungary": 84,
+"Turkey": 71,
+"Canada": 84,
+"Australia": 82,
+"New Zealand": 77,
+"Japan": 78,
+"China": 64,
+"Hong Kong": 67,
+"India": 62,
+"Malaysia": 82,
+"Philippines": 67,
+"Singapore": 83,
+"South Korea": 78,
+"Israel": 77,
+"Indonesia": 63,
+"Pakistan": 54,
+"Thailand": 72,
+"Kuwait": 72,
+"United Arab Emirates": 76,
+"Argentina": 71,
+"Brazil": 64,
+"Chile": 76,
+"Mexico": 72,
+"Panama": 71,
+"Venezuela": 52,
+"South Africa": 71,
+"Liberia": 48
+}
+
 def predict_y(data_dict):
 
     year = data_dict["year"]
     country_id = data_dict["country_id"] 
     region_id = data_dict["region_id"]
-    globalization_100 = data_dict["globalization_100"]
+    globalization_100 = globalization_dict[country_id]
     input_df = pd.DataFrame(
         {
             "year": [year],
@@ -51,7 +98,21 @@ def model_analysis():
 @app.route("/prediction", methods=["POST"])
 def prediction():
     data_dict = request.get_json()
-    fdi_prediction = predict_y(data_dict)
+
+    year = data_dict["year"]
+    country_id = data_dict["country_id"] 
+    region_id = data_dict["region_id"]
+    globalization_100 = data_dict["globalization_100"]
+    input_df = pd.DataFrame(
+        {
+            "year": [year],
+            "country_id": [country_id],
+            "region_id": [region_id],
+            "globalization_100": [globalization_100],
+        }
+    )
+
+    fdi_prediction = model.predict(input_df)[0]
 
     # fdi_prediction = predict_fdi_inflow(year, country_id, region_id, globalization_100)  
 
